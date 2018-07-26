8133f6b67f1eb74c02b8edfb86d23c9f69cbdf87
Pedro Romero <pedrojesusromeroortega@gmail.com>
2018-07-26 23:27:46 +0200

    Remove React-hot-loader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,8 @@
 import * as React from 'react';
-import { AppContainer } from 'react-hot-loader';
 import App from './app';
 
 const index = () => (
-  <AppContainer>
     <App />
-  </AppContainer>
 );
-
-if (module.hot) {
-  module.hot.accept('./app', () => {
-    index();
-  });
-}
 
 export default index;
