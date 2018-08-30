27fcc34842666b8883519b79db33c224623540ba
Day MP <dailymp@gmail.com>
2018-08-30 23:47:19 +0200

    Trying to add css styles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 const withTypescript = require('@zeit/next-typescript')
+
 module.exports = withTypescript({
   webpack(config, options) {
+    const { dev, isServer } = options
+  /*  const extractCSSPlugin = new ExtractTextPlugin({
+      filename: 'static/style.css',
+      disable: dev
+    })
+    config.module.rules.push({
+      test: /\.css$/,
+      use: cssLoaderConfig(extractCSSPlugin, {
+        cssModules,
+        dev,
+        isServer
+      })
+    })*/
     return config
   }
+
 })
--- src/common/components/map.tsx
+++ src/common/components/map.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document'
 
-interface Props {
+ interface Props {
   
 }
-
 export const Map = (props: Props) => (
   <div>
     <h1>Map</h1>    
   </div>
+ 
 );
+
+/*export  class Map extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="stylesheet" href="/_next/static/style.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}*/
