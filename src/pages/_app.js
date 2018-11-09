8bd192d1d541d2e56039514cafc49ef2f44ad5b2
nasdan <daniel.sanchez@lemoncode.net>
2018-11-09 13:48:09 +0100

    Load styles from chunk in dev mode

---
+++ src/pages/_app.js
@@ -0,0 +1,35 @@
+import React from "react";
+import App, { Container } from "next/app";
+import Router from "next/router";
+
+Router.events.on("routeChangeComplete", () => {
+  if (process.env.NODE_ENV !== "production") {
+    const els = document.querySelectorAll(
+      'link[href*="/_next/static/css/styles.chunk.css"]'
+    );
+    const timestamp = new Date().valueOf();
+    els[0].href = "/_next/static/css/styles.chunk.css?v=" + timestamp;
+  }
+});
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
